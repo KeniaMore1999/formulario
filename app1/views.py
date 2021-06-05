4f3d21ab12696d3e702d29b605f679c35e789e01
Kenia Moreira <alejandramoreia12@gmail.com>
2021-06-05 11:21:25 -0600

    Se realizaron algunos cambios en la tabla

--- app1/views.py
+++ app1/views.py
@@ -3,37 +3,41 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    formulario = [
-    {
-        'monto': '5000',
-        'tasa': '10',
-        'plazo': '12',
-        'cuota': '600',
-        'total': '200',
-        },
-        {   
-        'monto': '600',
-        'tasa': '2',
-        'plazo': '8',
-        'cuota': '100',
-        'total': '200',
-        },
+formulario = [
+{
+    'monto': '5000',
+    'tasa': '10',
+    'plazo': '12',
+    }
+] 
 
-    ]    
+def index(request):
+   
 
     if request.method == 'POST':
         monto  = request.POST.get('monto')
         tasa  = request.POST.get('tasa')
         plazo  = request.POST.get('plazo')
-        cuota  = request.POST.get('cuota')
-        total  = request.POST.get('total')
 
-        formulario
+        formulario.append(
+            {
+                'monto': monto,
+                'tasa': tasa,
+                'plazo': plazo,
+
+            }
+        )
+
+        ctx = {
+            'formulario': formulario
+        }
+        
+        return render(request, 'app1/index.html', ctx)
+        #return HttpResponse('Se agrego correctamente al formulario')
     else:
+        ctx = {
+            'formulario': formulario
+        }
 
-    ctx = {
-        'formulario': formulario
-    }
-
-    return render(request, 'app1/index.html', ctx)
+        return render(request, 'app1/index.html', ctx)
+        
